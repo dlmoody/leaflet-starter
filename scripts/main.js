6970207006f1e719a4a661cf7abbbabee550853d
David Moody <moody.david@gmail.com>
2015-08-11 20:50:57 +0000

    calling setup from the script
    Yeah

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,12 @@ main.js
 
 */
 
-function main(){
-    
-    var mainMessage = "this is the main method";
+function run(){
+    var map = L.map('map')
+                    .setView([
+                        37.996162679728116, 
+                        -80.0079345703125
+                        ], 7);
+                        
+    var layer = L.esri.basemapLayer('Streets').addTo(map);
 }
